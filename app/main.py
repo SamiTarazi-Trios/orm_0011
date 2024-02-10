cf4a71769bb3c38d86ad380b3eb725f451c7f16a
SamiTarazi-Trios <sami.tarazi@trios.com>
2024-02-09 20:33:15 -0500

    updates

--- app/main.py
+++ app/main.py
@@ -3,15 +3,22 @@ import models, crud, schemas
 from connect import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
+## todo 
+## add a function to get user that takes a parameter user id and execute below
+def getUser(userId):
+    db = SessionLocal()
+    user = crud.get_user(db=db, user_id=userId)
+    db.close()
+    return user
 
-def get_db():
-    db = SessionLocal() 
-    try:
-        yield db
-    finally:
-        db.close()
+## to do create another function that gets ALL users 
+def getUsers():
+    db = SessionLocal()
+    users = crud.get_users(db)
+    db.close
+    return users
 
-db = SessionLocal()
-user = crud.get_user(db=db, user_id=1)
-db.close()
-print(user)
+todo_users = getUsers()
+# print all the emails of my users 
+for todo_user in todo_users:
+    print(todo_user.email)
